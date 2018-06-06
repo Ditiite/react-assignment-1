fc1023c5fbdcb28af3f54b608559504b466ca85f
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-06-06 10:30:10 +0300

    Header part fixed

--- src/header/Header.js
+++ src/header/Header.js
@@ -8,12 +8,24 @@ export class Header extends Component {
         return (
             <header className="App-header">
                 <section className="content--1">
-                    <img alt="logo" src={headerImg} />
-                    <p>Powerfull simple with a</p>
+                    <img className="devices-header" alt="logo" src={headerImg} />
+                    
+                    <p className="to-upper-case">
+                        <span className="bolder">Powerfull simple</span> with a <br />
+                        <span className="bolder"> squeeky clean</span> design.
+                    </p>
+                    <p className="header-text">
+                        Find out how you can offer quick and powerful solutions to your customers now!
+                    </p>
+                    <button className="blue-btn header-btn">Learn more</button>
+        
                 </section>
                 <section className="content--2">
-                    <button>Get Started</button>
+                    <div className="small-triangle-blue"></div>
+                    <p className="to-upper-case">Create a <span className="bolder">Powerful</span>solution now!</p>
+                    <button className="white-btn">Get Started</button>
                 </section>
+                
             </header>
         );
     }
